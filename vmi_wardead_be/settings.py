01e7229a9be961fba308ee4134f8675693f6efd3
espadonne <espadonne@outlook.com>
2025-07-10 13:47:10 -0400

    add domains

--- vmi_wardead_be/settings.py
+++ vmi_wardead_be/settings.py
@@ -130,6 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://vmimemorial.com",
+    "https://www.vmimemorial.com",
 ]
 
 # REST Framework settings
